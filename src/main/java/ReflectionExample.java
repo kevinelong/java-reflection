575555595ca602072f2219e54b46376c14cc4781
Kevin Ernest Long <kevinelong@gmail.com>
2025-06-18 11:23:00 -0700

    wip

--- src/main/java/ReflectionExample.java
+++ src/main/java/ReflectionExample.java
@@ -1,23 +1,33 @@
 import java.lang.reflect.Method;
 
+class GreeterService {
+    public void sayHello(String name) {
+        System.out.println("Hello, " + name + "!");
+    }
+
+    public void anotherMethod() {
+        System.out.println("This is another method.");
+    }
+}
+
 public class ReflectionExample {
 
     public static void main(String[] args) {
         try {
             // 1. Get the Class object for a class
-            Class<?> myClass = MyClass.class; // Or Class.forName("ReflectionExample.MyClass");
+            Class<?> c = GreeterService.class; // Or Class.forName("ReflectionExample.MyClass");
 
             // 2. Get all public methods declared in the class
-            Method[] methods = myClass.getMethods();
+            Method[] methods = c.getMethods();
 
             System.out.println("Methods in MyClass:");
             for (Method method : methods) {
-                System.out.println("- " + method.getName());
+                System.out.println("\t - " + method.getName());
             }
 
             // 3. Invoke a specific method by name
-            Object instance = myClass.getDeclaredConstructor().newInstance(); // Create an instance of MyClass
-            Method specificMethod = myClass.getMethod("sayHello", String.class); // Get the 'sayHello' method
+            Object instance = c.getDeclaredConstructor().newInstance(); // Create an instance of MyClass
+            Method specificMethod = c.getMethod("sayHello", String.class); // Get the 'sayHello' method
 
             // Invoke the method on the instance with arguments
             specificMethod.invoke(instance, "World");
@@ -28,12 +38,3 @@ public class ReflectionExample {
     }
 }
 
-class MyClass {
-    public void sayHello(String name) {
-        System.out.println("Hello, " + name + "!");
-    }
-
-    public void anotherMethod() {
-        System.out.println("This is another method.");
-    }
-}
